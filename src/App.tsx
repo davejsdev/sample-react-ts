149ccc71635421fd26bbdd99c273a3577ce5019a
David Leger <david.leger@getkard.com>
2024-10-24 20:27:23 -0300

    feat: add daily calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { styled } from '@linaria/react';
-import { Heading } from './components/Heading';
-import Prose from './components/Prose/Prose';
-
-import ReactLogo from './assets/react.svg';
-import { Button } from './components/Button';
-import { Link } from './components/Link';
+import { formatDate } from './lib/utils/format';
+import { useState } from 'react';
 
 const Grid = styled.main`
   display: grid;
@@ -25,39 +21,128 @@ const Content = styled.div`
   padding: 5rem 0;
 `;
 
-const ButtonContainer = styled.div`
+const Today = () => {
+  return <h1>{formatDate(new Date())}</h1>;
+};
+
+const StyledHour = styled.div`
   display: flex;
-  gap: 1rem;
+  gap: 0.25rem;
+  border-bottom: 1px solid white;
+  height: 3rem;
+`;
+type CalEvent = {
+  id: number;
+  startTime: number;
+  endTime: number;
+  name: string;
+};
+
+const EventBlock = styled.div`
+  display: block;
+  color: black;
+  background-color: pink;
 `;
 
-function App() {
-  const onClick = () => {
-    alert('Hello!');
+// TODO add edit and delete handlers
+const Hour = ({ hour, events }: { hour: number; events: CalEvent[] }) => {
+  return (
+    <StyledHour>
+      <div>Hour {hour}</div>
+
+      {events.map((event) => {
+        const eventLength = event.endTime - event.startTime;
+        return (
+          <EventBlock
+            key={event.id}
+            style={{ height: `calc(4rem * ${eventLength})` }}
+          >
+            {event.name}
+          </EventBlock>
+        );
+      })}
+    </StyledHour>
+  );
+};
+
+// TODO add prop for default values for editing
+const Header = ({ onSubmit }: { onSubmit: (event: CalEvent) => void }) => {
+  const [startTime, setStartTime] = useState<number>();
+  const [endTime, setEndTime] = useState<number>();
+  const [name, setName] = useState('');
+
+  const submitHandler = (e: any) => {
+    e.preventDefault();
+    // TODO validation
+    if (startTime === undefined || endTime === undefined) {
+      return;
+    }
+
+    onSubmit({
+      id: Date.now(),
+      startTime,
+      endTime,
+      name,
+    });
   };
+
+  return (
+    <form onSubmit={submitHandler}>
+      {/* TODO add form labels */}
+      <input
+        name="startTime"
+        type="number"
+        required
+        value={startTime}
+        onChange={(e) => {
+          setStartTime(parseInt(e.target.value));
+        }}
+      />
+      <input
+        name="endTime"
+        type="number"
+        required
+        value={endTime}
+        onChange={(e) => {
+          setEndTime(parseInt(e.target.value));
+        }}
+      />
+      <input
+        name="name"
+        type="text"
+        required
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+function App() {
+  // TODO: generate all 24 hours
+  const hours = [1, 2, 3];
+  const [events, setEvents] = useState<CalEvent[]>([]);
 
   return (
     <Grid>
       <Content>
-        <ReactLogo />
-        <Heading level={1}>React + TypeScript</Heading>
-        <Prose>
-          <p>
-            This is a minimal setup for a React app with TypeScript, Linaria,
-            and SVGR. It&apos;s a starting point for building a modern web app
-            with a focus on performance and developer experience.
-          </p>
-          <p>
-            The project is set up with <code>vitest</code> for testing and{' '}
-            <code>prettier</code> for formatting. It includes a simple test and
-            a GitHub Actions workflow for continuous integration.
-          </p>
-        </Prose>
-        <ButtonContainer>
-          <Button onClick={onClick}>Click me!</Button>
-          <Link href="https://github.com/davidleger95/sample-react-ts" external>
-            View on GitHub
-          </Link>
-        </ButtonContainer>
+        <Today />
+        <Header
+          onSubmit={(event) => {
+            setEvents((prevEvents) => [...prevEvents, event]);
+          }}
+        />
+        {hours.map((hour) => {
+          const eventsForHour = events.filter(
+            (event) => event.startTime === hour
+          );
+
+          return <Hour key={hour} hour={hour} events={eventsForHour} />;
+        })}
+        <pre>{JSON.stringify(events, null, 2)}</pre>
       </Content>
     </Grid>
   );
--- src/lib/utils/format.ts
+++ src/lib/utils/format.ts
@@ -1,6 +1,7 @@
 const dateFormat = Intl.DateTimeFormat('en-CA', {
   month: 'short',
   year: 'numeric',
+  day: 'numeric'
 });
 
 export const formatDate = (date: Date) => dateFormat.format(date);
